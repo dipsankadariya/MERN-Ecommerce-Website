169093a84315181353f3fd9deb9a07f348344250
Dipsan Kadariya <dipsankadariya99@gmail.com>
2024-10-12 18:32:34 +0545

    minor changes

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -112,7 +112,7 @@ function Home() {
 
         {loading ? (
           <div className="text-center text-2xl font-bold text-gray-600">
-            Loading data, please wait...
+            Fetching data may take some time, please wait...
           </div>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
